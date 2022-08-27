dfca8ac25ad29da8def604ff5428ffb5951a8f43
jobala <japhethobalak@gmail.com>
2022-08-27 13:17:24 +0100

    refactor methods

--- internal/command/load.go
+++ internal/command/load.go
@@ -1,16 +1,11 @@
 package command
 
 import (
+	"reflect"
+	"strings"
+
 	"github.com/akamensky/argparse"
 )
-
-func addCommandToGroup[T any](name, description string, handler func(args T)) CliCommand[T] {
-	return CliCommand[T]{
-		Name:        name,
-		Description: description,
-		Handler:     handler,
-	}
-}
 
 func (c CliCommand[T]) Load(group string, parser *argparse.Parser) {
 	subparser := getSubParser(group, parser)
@@ -26,6 +21,35 @@ func (c CliCommand[T]) Load(group string, parser *argparse.Parser) {
 			handlerFunc: c.Handler,
 		},
 	})
+}
+
+func getCmdArgsFromHandler[T any](handlerFunc Handler[T]) map[string]string {
+	var argStruct T
+	args := make(map[string]string)
+
+	argReflection := reflect.TypeOf(argStruct)
+	for i := 0; i < argReflection.NumField(); i++ {
+		curField := argReflection.Field(i)
+		field := strings.ToLower(curField.Name)
+		args[field] = curField.Type.Name()
+	}
+
+	return args
+}
+
+func addArgsToCmd(args map[string]string, cmd *argparse.Command) map[string]any {
+	cmdArgs := make(map[string]any)
+
+	for field, dataType := range args {
+		switch dataType {
+		case "int":
+			cmdArgs[field] = cmd.Int("", field, nil)
+		case "string":
+			cmdArgs[field] = cmd.String("", field, nil)
+		}
+	}
+
+	return cmdArgs
 }
 
 type Handler[T any] func(args T)
--- internal/command/parser.go
+++ internal/command/parser.go
@@ -1,7 +1,6 @@
 package command
 
 import (
-	"reflect"
 	"strings"
 
 	"github.com/akamensky/argparse"
@@ -34,35 +33,6 @@ func getSubParser(group string, parser *argparse.Parser) *argparse.Command {
 	}
 
 	return subParsers[group]
-}
-
-func getCmdArgsFromHandler[T any](handlerFunc Handler[T]) map[string]string {
-	var argStruct T
-	args := make(map[string]string)
-
-	argReflection := reflect.TypeOf(argStruct)
-	for i := 0; i < argReflection.NumField(); i++ {
-		curField := argReflection.Field(i)
-		field := strings.ToLower(curField.Name)
-		args[field] = curField.Type.Name()
-	}
-
-	return args
-}
-
-func addArgsToCmd(args map[string]string, cmd *argparse.Command) map[string]any {
-	cmdArgs := make(map[string]any)
-
-	for field, dataType := range args {
-		switch dataType {
-		case "int":
-			cmdArgs[field] = cmd.Int("", field, nil)
-		case "string":
-			cmdArgs[field] = cmd.String("", field, nil)
-		}
-	}
-
-	return cmdArgs
 }
 
 type CommandItem struct {
