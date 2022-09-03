c7e6720001cecca823325b17e63f57956fdff2e1
jobala <japhethobalak@gmail.com>
2022-09-03 21:22:31 +0100

    chore: add one more test

--- internal/command/parser_test.go
+++ internal/command/parser_test.go
@@ -14,3 +14,14 @@ func TestGetSubParser_CreatesCommandTree(t *testing.T) {
 	assert.Equal(t, "mammals", subParser.GetParent().GetName())
 	assert.Equal(t, "placentilia", subParser.GetName())
 }
+
+func TestGetSubParser_ParentCanHaveChildren(t *testing.T) {
+	parser := NewParser("Demo", "Demo CLI")
+
+	cold := parser.getSubParser("mammals cold")
+	warm := parser.getSubParser("mammals warm")
+
+	assert.Equal(t, "mammals", cold.GetParent().GetName())
+	assert.Equal(t, "mammals", warm.GetParent().GetName())
+
+}
